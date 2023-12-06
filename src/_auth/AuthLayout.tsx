24efc7599c1a71edc200094bc8d0652af376d4b1
alexy piche <alexyautumn@gmail.com>
2023-12-06 12:07:27 -0500

    started adding the sign up form

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -13,7 +13,9 @@ const AuthLayout = () => {
         </section>
 
         <img 
-          src=''
+          src='/assets/images/side-img.svg'
+          alt='logo'
+          className='hidden md:block h-screen w-1/2 object-cover bg-no-repeat'
         />
       </>
     )}
