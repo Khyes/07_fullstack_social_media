cc125774148207425757e1a49795be0592ea1b3d
alexy piche <alexyautumn@gmail.com>
2023-12-06 11:11:49 -0500

    Initial Set up

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes , Route} from 'react-router-dom';
+
+import './globals.css';
+
+function App() {
+
+  return (
+   <main className='flex h-screen'>
+
+   </main>
+  )
+}
+
+export default App
